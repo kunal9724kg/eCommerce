bf4850bd4326d25c563664e8ae1a6bd2475f7561
Kunal Gupta <kunal9724kg@Kunals-MacBook-Air.local>
2020-06-25 00:12:49 +0530

    Server started successfully

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,12 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.use('/test', (req, res) => {
+
+    res.send("Server is running....");
+});
+
+exports.api = functions.https.onRequest(app);
