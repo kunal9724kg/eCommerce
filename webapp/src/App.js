3ba5f3c11e1ce9725c6b91f31c6eff13b8147931
Mudit Jain <ciphereck@gmail.com>
2020-07-04 22:24:44 +0530

    Adding Basic Routes[WebApp]
    Signed-off-by: Mudit Jain <ciphereck@gmail.com>

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import Admin from './components/admin/Admin';
+import NotFound from './components/NotFound/NotFound'
 
 function App() {
   return (
-    <div>
-      <h1>E-Commerce</h1>
-    </div>
+    <BrowserRouter>
+      <div>
+        <React.Suspense fallback={<div>Loading......</div>}>
+          
+          <Switch>
+            <Route exact path="/admin" component={Admin} />
+            <Route path="/*" component={NotFound} />
+          </Switch>
+        
+        </React.Suspense>
+      </div>
+    </BrowserRouter>
   );
 }
 
