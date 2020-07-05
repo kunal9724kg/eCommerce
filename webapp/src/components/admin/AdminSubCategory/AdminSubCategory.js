45802518a0809787480563b847be8c73d6b2938c
Mudit Jain <ciphereck@gmail.com>
2020-07-05 13:12:42 +0530

    Fetching SubCategories in React Hooks
    Signed-off-by: Mudit Jain <ciphereck@gmail.com>

--- webapp/src/components/admin/AdminSubCategory/AdminSubCategory.js
+++ webapp/src/components/admin/AdminSubCategory/AdminSubCategory.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import {getSubCategories} from '../../../services/util'
+import React, {useState, useEffect} from 'react';
+import {getSubCategories} from '../../../services/util';
 
-function AdminSubCategory() {
-  getSubCategories()
-    .then(subcategories => console.log(subcategories))
-  return (
-    <h1>Admin SubCategory....</h1>
-  );
+const AdminSubCategory = () => {
+  const [loading, setLoading] = useState(true)
+  const [subCategories, setSubCategories] = useState("Hi")
+
+  async function fetchSubCategories() {
+    const res = await getSubCategories()
+    if(res != null) {
+      setSubCategories(res)
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchSubCategories()
+  }, [])
+
+  if(loading) {
+    return (
+      <h1>Loading.....</h1>
+    )
+  } else {
+    return (
+      <div>
+        {JSON.stringify(subCategories)}
+      </div>
+    )
+  }
 }
 
 export default AdminSubCategory;
