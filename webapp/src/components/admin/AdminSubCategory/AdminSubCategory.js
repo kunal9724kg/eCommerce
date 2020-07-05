110700bc4f546237f058fa74454263db7fcfaadd
Mudit Jain <ciphereck@gmail.com>
2020-07-05 14:03:29 +0530

    Drop Down for SubCategory is Working
    Signed-off-by: Mudit Jain <ciphereck@gmail.com>

--- webapp/src/components/admin/AdminSubCategory/AdminSubCategory.js
+++ webapp/src/components/admin/AdminSubCategory/AdminSubCategory.js
@@ -1,13 +1,26 @@
 import React, {useState, useEffect} from 'react';
+import { select } from '@material-ui/core';
 import {getSubCategories} from '../../../services/util';
 
 const AdminSubCategory = () => {
+  let choseSubCategory = {
+    "id": "Choose",
+    "name": "Choose Sub Category"
+  }
+  let newSubCategoryOption = {
+    "id": "New",
+    "name": "New Sub Category"
+  }
   const [loading, setLoading] = useState(true)
   const [subCategories, setSubCategories] = useState("Hi")
+  const [subCategory, setSubCategory] = useState(0)
+  
 
   async function fetchSubCategories() {
-    const res = await getSubCategories()
+    let res = await getSubCategories()
     if(res != null) {
+      res = [choseSubCategory].concat(res)
+      res.push(newSubCategoryOption)
       setSubCategories(res)
     }
     setLoading(false)
@@ -17,6 +30,10 @@ const AdminSubCategory = () => {
     fetchSubCategories()
   }, [])
 
+  useEffect(() => {
+    console.log(subCategories[subCategory])
+  }, [subCategory])
+
   if(loading) {
     return (
       <h1>Loading.....</h1>
@@ -24,7 +41,14 @@ const AdminSubCategory = () => {
   } else {
     return (
       <div>
-        {JSON.stringify(subCategories)}
+        <select value={subCategory} onChange={(event) => setSubCategory(event.target.value)}>
+          {subCategories
+            .map((subCategory, idx) => <option 
+                                          key={subCategory.id} 
+                                          value={idx}>
+                                        {subCategory.name}
+                                      </option>)}
+        </select>
       </div>
     )
   }
